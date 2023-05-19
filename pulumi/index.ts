07e346d5404cc248ec1a31677fc882a4db03b863
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2023-05-19 09:53:38 +0200

    S3 code bucket versioning + fixes for running as AWS Lambda

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-
 import {dataBucket} from './src/s3-data';
 import {lambdaFunctionUrl} from './src/lambda';
+import {codeJarObject} from "./src/s3-code";
 
 export const bucketName = dataBucket.id;
 export const bucketDomainName = dataBucket.bucketDomainName;
 export const functionUrl = lambdaFunctionUrl.functionUrl
+export const codeJarVersionId = codeJarObject.versionId
--- pulumi/src/s3-code.ts
+++ pulumi/src/s3-code.ts
@@ -1,9 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-export const codeBucket = new aws.s3.Bucket("swisshikes-generate-code");
+export const codeBucket = new aws.s3.Bucket(
+    "swisshikes-generate-code",
+    {
+        versioning: {
+            enabled: true,
+        }
+    }
+);
 
-export const codeJarObject = new aws.s3.BucketObject("codeJar", {
+export const codeJarObject = new aws.s3.BucketObjectv2("codeJar", {
     key: "swisshikes-generate.jar",
     bucket: codeBucket.id,
     source: new pulumi.asset.FileAsset("../target/scala-2.13/swisshikes-generate.jar")
