8c2a031dd4bfa5d2c2db9682913a442f2f93509c
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2023-07-24 22:49:26 +0200

    fix issue with redundant decimal point ("8." instead of "8" or "8.0")

--- src/main/scala/be/stijnvermeeren/swisshikesgenerate/Coord.scala
+++ src/main/scala/be/stijnvermeeren/swisshikesgenerate/Coord.scala
@@ -10,6 +10,6 @@ case class Coord(lon: Double, lat: Double) {
 object Coord {
   def formatPart(value: Double): String = {
     val output = f"$value%.5f" // five decimal places after the point
-    output.reverse.dropWhile(_ == '0').reverse // drop zeroes at the end
+    output.reverse.dropWhile(_ == '0').dropWhile(_ == '.').reverse // drop zeroes at the end
   }
 }
